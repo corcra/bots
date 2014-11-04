3d1b8051b6c5523aebc0dee5bd0aa686d21f84f9
Shawn Nock <nock@nocko.se>
2014-11-04 10:51:55 -0500

    Moves multipart tweeting to twit callback to ensure order

--- emmagoldbot/emma_book.js
+++ emmagoldbot/emma_book.js
@@ -8,30 +8,42 @@ var T = new Twit(require('./emma_config.js'));
 
 var data = fs.readFileSync('./pg2162_clean.txt.twit','utf8').toString().split('\n');
 var linenum = 0;
+var tweet_parts, i=0;
 var re = /\(([0-9]+)\/([0-9]+)\)$/;
+var quote = data[linenum];
+
+function post_next_if_multi(error, response) {
+    if (response) {
+	console.log('~\n!!\n~anarchy~\n!!\n~');
+	console.log(linenum);
+	linenum += 1;
+	quote = data[linenum];
+	if (i < tweet_parts) {
+	    i+=1;
+	    T.post('statuses/update', { status: quote }, post_next_if_multi);
+	} else {
+	    i=0;
+	}
+    }
+    if (error) {
+	console.log('Error with twitter:',error);
+    }
+}
 
 function readbook() {
-    var num_tweets = 1, i;
     var match_array;
-    quote = data[linenum];
-    match_array = quote.match(re);
-    if (match_array != null)
-	num_tweets = match_array[2];
-    for(i=0; i<num_tweets; i++) {
-	console.log(linenum,quote);
-	T.post('statuses/update', { status: quote }, function (error, response) {
-            if (response) {
-		console.log('~\n!!\n~anarchy~\n!!\n~');
-		console.log(linenum);
-            }
-            // if error with twitter call
-            if (error) {
-		console.log('Error with twitter:',error);
-            }
-	});
-	linenum = linenum + 1;
-	quote = data[linenum];
+    if (!quote) {
+	return;
     }
+    match_array = quote.match(re);
+    if (match_array != null) {
+	tweet_parts = match_array[2]-1;
+        i = 0;
+    }
+    else {
+	tweet_parts = 1;
+    }
+    T.post('statuses/update', { status: quote }, post_next_if_multi);
 }
 
 readbook();
