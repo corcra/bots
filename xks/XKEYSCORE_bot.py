200eb5dc7c2c843779b3f8b6141ccae20c92bdc8
Stephanie Hyland <steph.hyland@gmail.com>
2015-08-08 13:24:20 -0400

    derp

--- xks/XKEYSCORE_bot.py
+++ xks/XKEYSCORE_bot.py
@@ -8,7 +8,7 @@ selector_terms = load('terms.npy').item()
 
 # --- get set up API --- #
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_torkn(access_token, access_token_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # --- ... things ... --- #
