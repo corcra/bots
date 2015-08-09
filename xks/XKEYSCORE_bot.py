ff205057d9956d438bf74e7be10741a0f13e0828
Stephanie Hyland <steph.hyland@gmail.com>
2015-08-08 21:59:25 -0400

    searches for tweets containing selector terms, retweets, sleeps

--- xks/XKEYSCORE_bot.py
+++ xks/XKEYSCORE_bot.py
@@ -3,12 +3,28 @@
 import tweepy
 from numpy import load
 from creds import consumer_key, consumer_secret, access_token, access_token_secret
+from random import sample
+from time import sleep
 
 selector_terms = load('terms.npy').item()
 
-# --- get set up API --- #
+# --- set up API --- #
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# --- ... things ... --- #
+# --- search for tweets containing a random selector term --- #
+def retweet_selector(selector):
+    # grab a tweet containing this term
+    tweet = api.search(q=selector).pop()
+    print 'Retweeting', tweet.text, 'from user', tweet.user.screen_name, 'because',
+    print 'it contains selector term:', selector
+    # retweet it
+    api.retweet(tweet.id)
+
+
+# --- main etc ? --- #
+while True:
+    selector = sample(selector_terms, 1)[0]
+    retweet_selector(selector)
+    sleep(15*60)
